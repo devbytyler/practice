4b8617a02e484e5f2f9e6b0021fd0c2b54511392
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-16 11:21:58 -0700

    added notes ont he select method

--- class/oddeven.rb
+++ class/oddeven.rb
@@ -7,3 +7,8 @@ if (num % 2 == 0)
 else
   puts "Number is odd."
 end
+
+
+(1..10).to_a.select(&:even?) #means that you are passing a block. bypasses having an iterator. apply a method to each value in an array or range
+
+(1..10).to_a.select(|x| x.even?)
