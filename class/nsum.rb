a4b2b3d26f2e1d258f12e79261d0e1f251ef4ff5
tylerstephens814 <tylerstephens814@gmail.com>
2017-01-16 09:46:18 -0700

    Spacing adjust

--- class/nsum.rb
+++ class/nsum.rb
@@ -5,21 +5,22 @@ class Integer
 end
 
 
-
-
+#Method 1 - Using .reduce
 print "Input a number: "
 num = gets.chomp
 sum = (1..num.to_i).reduce(:+) || 1
 puts sum
 
 
+#Method 2 - Using .each and a block (Does not work with zero)
 print "Input a number: "
 n = gets.chomp
 sum2 = 0
 (1..n.to_i).each {|num2| sum2 += num2}
 puts sum2
 
+
+#Method 3 - Extending the Integer class
 print "Input a number: "
 num = gets.chomp.to_i
-
 puts num.fact
